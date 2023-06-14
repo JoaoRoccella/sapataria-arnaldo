ca7781b8ca75ba463ea7524a20e95dc3e4f6aa3d
Joao Roccella <joao.marcos@sp.senai.br>
2023-06-14 16:19:01 -0300

    Criando comandos e afirmações

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('isNotInViewport', element => {
+    cy.get(element).should($el => {
+        const bottom = Cypress.$(cy.state('window')).height()
+        const rect = $el[0].getBoundingClientRect()
+
+        expect(rect.top).to.be.greaterThan(bottom)
+        expect(rect.bottom).to.be.greaterThan(bottom)
+        expect(rect.top).to.be.greaterThan(bottom)
+        expect(rect.bottom).to.be.greaterThan(bottom)
+    })
+})
+
+Cypress.Commands.add('isInViewport', element => {
+    cy.get(element).should($el => {
+        const bottom = Cypress.$(cy.state('window')).height()
+        const rect = $el[0].getBoundingClientRect()
+
+        expect(rect.top).not.to.be.greaterThan(bottom)
+        expect(rect.bottom).not.to.be.greaterThan(bottom)
+        expect(rect.top).not.to.be.greaterThan(bottom)
+        expect(rect.bottom).not.to.be.greaterThan(bottom)
+    })
+})
